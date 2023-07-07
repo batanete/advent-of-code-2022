032581dc8b50a5ccd84b09c6ae0fe05217e4cc4b
João Batanete <joao.batanete@mercedes-benz.io>
2023-07-07 17:26:17 +0100

    Add day4 ex1 and ex2 solutions

---
+++ day4/ex2/ex2.py
@@ -0,0 +1,29 @@
+# read input
+
+INPUT_PATH = './day4/input.txt'
+
+with open(INPUT_PATH, 'r') as f:
+    lines = f.readlines()
+    # remove line endings
+    lines = [line[:-1] for line in lines]
+
+# solution
+
+def intersect(r1, r2):
+    return (r2[0] >= r1[0] and r2[0] <= r1[1]) or (r2[1] >= r1[0] and r2[1] <= r1[1])
+
+matches = 0
+
+for line in lines:
+    r1_raw, r2_raw = line.split(',')
+    
+    r1_raw = r1_raw.split('-')
+    r1 = (int(r1_raw[0]), int(r1_raw[1]))
+
+    r2_raw = r2_raw.split('-')
+    r2 = (int(r2_raw[0]), int(r2_raw[1]))
+
+    if intersect(r1, r2) or intersect(r2, r1):
+        matches += 1
+
+print(matches)
